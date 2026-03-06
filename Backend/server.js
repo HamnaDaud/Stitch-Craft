ebf8ead1b238717bcbe37ae651fc390decc81fef
Hamna Daud <hamnadaud7@gmail.com>
2026-03-06 21:34:35 +0500

    MongoDB setup and connection

--- Backend/server.js
+++ Backend/server.js
@@ -1,27 +1,38 @@
-const express = require("express");
+import express from 'express';
+import dotenv from 'dotenv';
+
+import mongoose from 'mongoose';
+import dns from 'dns';
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+dotenv.config();
 
 const app = express();
-const PORT = 5000;
 
 // Middleware
+
 app.use(express.json());
-
-
-// Healthcheck API
 app.get("/health", (req, res) => {
-    res.status(200).json({
-        status: "OK",
-        message: "Server is running",
-        timestamp: new Date()
+  res.status(200).json({message: "Healthy"})
+});
+
+app.use((req, res, next) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
+
+
+// MongoDB Connection
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
     });
-});
-
-// Test Route
-app.get("/", (req, res) => {
-    res.send("🚀 Backend server is running");
-});
-
-// Start Server
-app.listen(PORT, () => {
-    console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+  })
+  .catch((err) => {
+    console.error('MongoDB Connection Error:', err.message);
+    process.exit(1);
+  });
