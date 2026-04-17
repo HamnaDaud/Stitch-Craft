6c4c77ecf95c0f3e3c465a5025534c7321055e67
Hamna Daud <hamnadaud7@gmail.com>
2026-04-17 19:14:03 +0500

    Middlewares Added

---
+++ Backend/middlewares/Role.middleware.js
@@ -0,0 +1,14 @@
+export const authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!req.user) {
+      return res.status(401).json({ message: "Not authorized, no user found" });
+    }
+
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({ 
+        message: `Access denied. Role '${req.user.role}' is not authorized to perform this action.` 
+      });
+    }
+    next();
+  };
+};
